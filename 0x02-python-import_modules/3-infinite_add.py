41c000c4a09e0e2232884a89f8dfa955360e6a18
lidu-1 <lilujoli65@gmail.com>
2022-06-04 04:28:27 -0700

    updated...

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from sys import argv
 if __name__ == '__main__':
-    num_arg = 0
-    for i in argv[1:]:
-        num_arg = num_arg + int(i)
-    print(num_arg)
+    numarg = 0
+    for num in argv[1:]:
+        numarg = numarg + int(num)
+    print(numarg)
